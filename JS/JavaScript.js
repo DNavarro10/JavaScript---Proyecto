392a5e4eef1169083ebf37545e2b35a32fd0db09
DNavarro10 <luis_d_100@hotmail.com>
2019-06-05 00:12:27 -0600

    Preparando contenido

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -4,11 +4,41 @@ $(document).ready(function(){
     
     $('.bxslider').bxSlider({
         auto: true,
-        
         stopAutoOnClick: true,
         captions: true,
         responsive: true,
         controls: true,
         
       });
+
+     
+      //formato para fechas
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let today  = new Date();
+
+       //POST
+      let post = [
+        	{
+                "title" : "Titulo 1 Aventura",
+                "date": today.toLocaleDateString("es-ES", options),
+                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+            },
+            {
+                "title" : "Titulo 2 Viaje",
+                "date": today.toLocaleDateString("es-ES", options),
+                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+            },
+            {
+                "title" : "Titulo 1 Descanso",
+                "date": today.toLocaleDateString("es-ES", options),
+                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+            },
+            {
+                "title" : "Titulo 1 Comidas",
+                "date": today.toLocaleDateString("es-ES", options),
+                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+            },
+      ];
+
+      console.log(post);
 });
