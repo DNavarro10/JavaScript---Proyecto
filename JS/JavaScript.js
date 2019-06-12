ad895b4027c279709089d610da87a1a4214f7dc3
DNavarro10 <luis_d_100@hotmail.com>
2019-06-11 20:39:01 -0600

    Boton de subir listo

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,94 +1,114 @@
 'use strict'
 
-$(document).ready(function(){
-    //Nav  active 
-    let lista = $('nav li');
-        lista.click(function() {
-        // remove classes from all
-        lista.removeClass("active");
-        // add class to the one we clicked
-        $(this).addClass("active");
-     });
-      //selector
-    
-      let selector = $('.selector div');
-        selector.click(function() {
-        // remove classes from all
-        selector.removeClass("activa");
-        // add class to the one we clicked
-        $(this).addClass("activa");
-     });
+$(document).ready(function () {
 
-      //temas
+  navSelect();
+  selectTheme();
+  theme();
+  sliderBox();
+  parallax();
+  contenido();
 
-      let theme = $('#theme');
+  irArriba();
 
-      $('#theme-1').click(function(){
-        theme.attr("href","CSS/theme/color1.css");
-      });
-      $('#theme-2').click(function(){
-        theme.attr("href","CSS/theme/color2.css");
-      });
-      $('#theme-3').click(function(){
-        theme.attr("href","CSS/theme/color3.css");
-      });
-     
-      
-    //slider
-    let slider = $('.bxslider');
-    slider.bxSlider({
-        auto: true,
-        stopAutoOnClick: true,
-        captions: true,
-        responsive: true,
-        controls: true,
-        
-      });
-    
-    	//parallax
-	$(window).scroll(function(){
-		let barra = $(window).scrollTop();
-		let posicion = barra * 0.35;
+});
 
-		$('body').css({
-			'background-position': '0 -' + posicion + 'px'
-		});
+function navSelect() {
+  //Nav  active 
+  let lista = $('nav li');
+  lista.click(function () {
+    // remove classes from all
+    lista.removeClass("active");
+    // add class to the one we clicked
+    $(this).addClass("active");
+  });
+};
+
+function selectTheme() {
+  //selector
+  let selector = $('.selector div');
+  selector.click(function () {
+    // remove classes from all
+    selector.removeClass("activa");
+    // add class to the one we clicked
+    $(this).addClass("activa");
+  });
+};
+//temas
+function theme() {
+  let theme = $('#theme');
+
+  $('#theme-1').click(function () {
+    theme.attr("href", "CSS/theme/color1.css");
+  });
+  $('#theme-2').click(function () {
+    theme.attr("href", "CSS/theme/color2.css");
+  });
+  $('#theme-3').click(function () {
+    theme.attr("href", "CSS/theme/color3.css");
+  });
+
+};
+function sliderBox() {
+  //slider
+  let slider = $('.bxslider');
+  slider.bxSlider({
+    auto: true,
+    stopAutoOnClick: true,
+    captions: true,
+    responsive: true,
+    controls: true,
+
+  });
+};
+
+function parallax() {
+  //parallax
+  $(window).scroll(function () {
+    let barra = $(window).scrollTop();
+    let posicion = barra * 0.35;
+
+    $('body').css({
+      'background-position': '0 -' + posicion + 'px'
     });
-    
-    	
-      //formato para fechas
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    let today  = new Date();
+  });
 
-       //POST
-      let post = [
-        	{
-                "title" : "Titulo 1 Aventura",
-                "date": today.toLocaleDateString("es-ES", options),
-                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
-            },
-            {
-                "title" : "Titulo 2 Viaje",
-                "date": today.toLocaleDateString("es-ES", options),
-                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
-            },
-            {
-                "title" : "Titulo 3 Descanso",
-                "date": today.toLocaleDateString("es-ES", options),
-                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
-            },
-            {
-                "title" : "Titulo 4 Comidas",
-                "date": today.toLocaleDateString("es-ES", options),
-                "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
-            },
-      ];
+};
 
-      //usar el objeto/joson
-      post.forEach((item, index) =>{
-        let contenido = $('#posts'); 
-        let articulo = 
-        `<div class="post" id="posts">
+function contenido() {
+  //formato para fechas
+  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  let today = new Date();
+
+  //POST
+  let post = [
+    {
+      "title": "Titulo 1 Aventura",
+      "date": today.toLocaleDateString("es-ES", options),
+      "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+    },
+    {
+      "title": "Titulo 2 Viaje",
+      "date": today.toLocaleDateString("es-ES", options),
+      "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+    },
+    {
+      "title": "Titulo 3 Descanso",
+      "date": today.toLocaleDateString("es-ES", options),
+      "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+    },
+    {
+      "title": "Titulo 4 Comidas",
+      "date": today.toLocaleDateString("es-ES", options),
+      "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt earum provident eius beatae accusantium consequatur! Quas, quibusdam animi ducimus, rem perspiciatis harum quod accusamus consectetur deleniti, veritatis cum rerum."
+    },
+  ];
+
+  //usar el objeto/joson
+  post.forEach((item, index) => {
+    let contenido = $('#posts');
+    let articulo =
+      `<div class="post" id="posts">
         <article id="articulo">
             <h2>${item.title}</h2>
             <span id="date">Publicado: ${item.date}</span>
@@ -97,12 +117,24 @@ $(document).ready(function(){
             <hr>
         </article>
         </div>`;
-      
-      //jquery para añadir divs
-      
-      contenido.append(articulo);
-      });
 
-    
-});
+    //jquery para añadir divs
 
+    contenido.append(articulo);
+  });
+};
+
+
+//boton de subir
+function irArriba() {
+  let arriba =  $('.ir-arriba');
+  let cuerpo = $('body,html');
+
+  arriba.click(function (e) {
+    e.preventDefault();
+    cuerpo.animate({ scrollTop: '0px' }, 1000); });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) { arriba.fadeIn().slideDown(600); } else { arriba.fadeOut().slideUp(600); }
+  });
+ 
+}
